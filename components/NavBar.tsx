eb5ef55ae21d957bd99db9f8b518e6b049a157d2
sahasruhh <sahasrajoginipally0@gmail.com>
2025-03-20 20:09:54 -0400

    navbar

---
+++ components/NavBar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function NavBar() {
+  return (
+    <nav>
+        <div className=" container rounded-2xl flex sticky pt-10 bg-gray-400">
+
+        </div>
+    </nav>
+  );
+}
