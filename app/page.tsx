88b5888779571d35666bc46a2222056e1abf00a2
gurujaya <86539998+gurujaya@users.noreply.github.com>
2025-03-23 13:29:35 -0400

    navbar progress

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,7 @@
-
-
 export default function Home() {
   return (
-    <div>
-      
+    <div className="text-red-600 font-bold text-3xl">
+      <h1>Home Page</h1>
     </div>
   );
 }
--- components/NavBar.tsx
+++ components/NavBar.tsx
@@ -1,11 +1,25 @@
-import React from "react";
+import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+import { NAV_LINKS } from '@/constants';
+import Button from './Button';
 
 export default function NavBar() {
   return (
-    <nav>
-        <div className=" container rounded-2xl flex sticky pt-10 bg-gray-400">
-
-        </div>
+    <nav className="flex sticky max-container padding-container  z-30 py-2 bg-[#D9D9D9] rounded-full mt-4">
+      <div className="flex items-center w-full justify-between ">
+        <Link href="/">
+          <h1 className='font-league font-[1000] text-3xl tracking-[-.08em]'>posturly.</h1>
+        </Link>
+        <ul className='hidden h-full gap-12 lg:flex absolute left-1/2 transform -translate-x-1/2 pt-7'> 
+          {NAV_LINKS.map((link) => (
+            <Link href={link.href} key={link.key} className='text-lg font-light text-grey-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold'>
+              {link.label}
+            </Link>
+          ))}
+        </ul>
+      </div>
     </nav>
   );
 }
+
